3186d65097a80baa9439c207719a9a74921a70e9
nilutpol8722 <nilutpolborpatra80@gmail.com>
2024-03-31 21:55:28 +0530

    Desktop/tea-p/.git

---
+++ src/app/employee/page.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Layout from "@/components/Layout"
+import Typography from '@mui/material/Typography';
+export default function Home() {
+  return (
+    <main>
+      <Layout>
+
+      </Layout>
+    </main>
+  
+  );
+}
